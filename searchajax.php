24f7e66b277489ec624fe94c203ed594a662d944
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-07-06 23:31:34 +0700

    update searchajax.php    + remove style:color of tag a

--- searchajax.php
+++ searchajax.php
@@ -8,7 +8,7 @@ if (isset($_GET['search'])){
     $result = query($sql);
     if (mysqli_num_rows($result) > 0){
         while ($row = mysqli_fetch_assoc($result)){
-            $output .= "<li><a style='color: white;'  href='item.php?id=".$row['product_id']."&loai=".$row['product_category_id']."'>" .  $row['product_title'] . "</a></li>" ;
+            $output .= "<li><a href='item.php?id=".$row['product_id']."&loai=".$row['product_category_id']."'>" .  $row['product_title'] . "</a></li>" ;
         }
     }
     echo $output;

f37c67734754738847342d1fcf91cd1e969aedfa
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-07-06 18:10:09 +0700

    add searchajax.php    + function search ajax page

---
+++ searchajax.php
@@ -0,0 +1,20 @@
+<?php
+require_once ("resources/config.php");
+$output = '';
+if (isset($_GET['search'])){
+    $search = escape_string($_GET['search']);
+    $sql = "SELECT product_id, product_title, product_category_id FROM products 
+            WHERE product_title LIKE '%". $search . "%' ";
+    $result = query($sql);
+    if (mysqli_num_rows($result) > 0){
+        while ($row = mysqli_fetch_assoc($result)){
+            $output .= "<li><a style='color: white;'  href='item.php?id=".$row['product_id']."&loai=".$row['product_category_id']."'>" .  $row['product_title'] . "</a></li>" ;
+        }
+    }
+    echo $output;
+}else{
+    $output = '';
+}
+
+
+

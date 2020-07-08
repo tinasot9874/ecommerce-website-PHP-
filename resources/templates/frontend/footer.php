a1ba7df14fb625359467fdd5ec66cafc92497c6e
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-07-08 23:50:24 +0700

    update footer.php   + add form modal

--- resources/templates/frontend/footer.php
+++ resources/templates/frontend/footer.php
@@ -40,7 +40,7 @@
 <!-- Modal -->
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
      aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-dialog " role="document" style="width: fit-content;">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLongTitle">Theo dỗi đơn hàng</h5>
@@ -49,18 +49,22 @@
                 </button>
             </div>
             <div class="modal-body">
+                <div id="inputemail" class="">
                     <div class="form-group">
                         <label for="email-tracking" class="col-form-label">Nhập email đặt hàng của bạn:</label>
-                        <input type="email" name="email" placeholder="Email của bạn" required/>
-                        <button type="submit" class="btn-track-order"><i class="ion-search"></i></button>
+                        <input id="track-order-email" type="text" name="email" placeholder="Email của bạn" required/>
+                        <button id="track-order-submit" type="submit" class="btn-track-order"><i class="ion-search"></i></button>
                     </div>
                     <div class="form-group">
-                        <label for="message-text" class="col-form-label">Message:</label>
-                        <textarea class="form-control" id="message-text"></textarea>
+                        <p style="color: red;" id="msg-trackorder"></p>
                     </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Thoát</button>
+                </div>
+
+                    <div id="track-order-result" class="form-group">
+
+
+
+                    </div>
             </div>
         </div>
     </div>
